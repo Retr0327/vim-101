845f2b8b2dddff9690b74d623fc0046c6f4800dd
Retr0327 <philcoke35@gmail.com>
2022-03-24 23:28:31 +0800

    Update order_system

--- practice/order_system.py
+++ practice/order_system.py
@@ -1,19 +1,17 @@
 class Order:
     def __init__(self):
         self.items = []
-        self.quantities = []
         self.prices = []
         self.status = "not_paid"
 
-    def add_online_class(self, name, quantity, price):
+    def add_online_class(self, name, price):
         self.items.append(name)
-        self.quantities.append(quantity)
         self.prices.append(price)
 
     def total_price(self):
         total = 0
         for i in range(len(self.prices)):
-            total += self.quantities[i] * self.prices[i]
+            total += self.prices[i]
         return total
 
     def pay(self, payment_type, security_code):
@@ -32,9 +30,9 @@ class Order:
 
 
 order = Order()
-order.add_online_class("Ray's udemy class", 1, 50000)
-order.add_online_class("Willy's codecademy class", 1, 50000)
-order.add_online_class("Prof. Hsieh's DataCamp class", 1, 99999)
+order.add_online_class("Ray's udemy class", 50000)
+order.add_online_class("Willy's codecademy class", 50000)
+order.add_online_class("Prof. Hsieh's DataCamp class", 99999)
 
 print(order.total_price())
 order.pay("debit", "123456")
